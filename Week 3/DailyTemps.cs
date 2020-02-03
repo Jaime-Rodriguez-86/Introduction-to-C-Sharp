6ae86e5e3faf91b64b0a83d73c7d99699c3f81dc
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-03 18:40:54 -0500

    Update DailyTemps.cs

--- Week 3/DailyTemps.cs
+++ Week 3/DailyTemps.cs
@@ -1,3 +1,15 @@
+/* 
+* Purpose: Receive temperatures from user and
+* give the number of inputs with the average temperature
+*
+* To compile the program:
+* csc DailyTemps.cs
+*
+* To execute the program:
+* DailyTemps.exe (.exe optional)
+*
+*/
+ 
 using System;
 using static System.Console;
 
@@ -7,6 +19,7 @@ namespace DailyTemps
 	{
 		public static void Main()
 		{
+			//Define variables
 			const int LOW = -20;
 			const int HIGH = 130;
 			int sum = 0;
@@ -19,21 +32,25 @@ namespace DailyTemps
 		
 			temperatureString = Console.ReadLine();
 			temperature = Convert.ToInt32(temperatureString);
-			  
+			
+			//Run after receiving user input
 			while(temperatureString != "##")
 			{
+				//If user input is invalid
 				while(temperature < LOW || temperature > HIGH)
 				{
 					Console.Write("Input of {0} is not a valid temperature. Enter a valid temperature.", temperatureString);
 					temperatureString = Console.ReadLine();
 					temperature = Convert.ToInt32(temperatureString);
 				}
+			//If user input is valid
 			sum += temperature;
 			count++;
 			Console.Write ("Enter a temperature between -20 and 130 degrees Fahrenheit. When finished enter ##.");
 			temperatureString = Console.ReadLine();
 			}
 		
+			//Run to finish and terminate program
 			if(temperatureString == "##")
 			{
 				average = sum / count;
