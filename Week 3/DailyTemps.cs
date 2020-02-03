92caeebb6eb9c4e6e9485a5b124cad363fd8344c
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-03 17:47:44 -0500

    Update DailyTemps.cs

--- Week 3/DailyTemps.cs
+++ Week 3/DailyTemps.cs
@@ -1,42 +1,44 @@
 using System;
-					
-public class Program
-{
-	public static void Main()
+using static System.Console;
+
+namespace DailyTemps
+{				
+	public class Program
 	{
-		const int LOW = -20;
-		const int HIGH = 130;
-		int sum = 0;
-		int count = 0;
-		double average = 0;
-       		string temperatureString;
-		int temperature;
+		public static void Main()
+		{
+			const int LOW = -20;
+			const int HIGH = 130;
+			int sum = 0;
+			int count = 0;
+			double average = 0;
+       			string temperatureString;
+			int temperature;
 		
-		Console.Write ("Enter a temperature between -20 and 130 degrees Fahrenheit. When finished enter ##.");
+			Console.Write ("Enter a temperature between -20 and 130 degrees Fahrenheit. When finished enter ##. ");
 		
-		temperatureString = Console.ReadLine();
-		temperature = Convert.ToInt32(temperatureString);
+			temperatureString = Console.ReadLine();
+			temperature = Convert.ToInt32(temperatureString);
 			  
-		while(temperatureString != "##")
-		{
-			while(temperature < LOW || temperature > HIGH)
+			while(temperatureString != "##")
 			{
-				Console.Write("Input of {0} is not a valid temperature. Enter a valid temperature.", temperatureString);
-				temperatureString = Console.ReadLine();
-				temperature = Convert.ToInt32(temperatureString);
+				while(temperature < LOW || temperature > HIGH)
+				{
+					Console.Write("Input of {0} is not a valid temperature. Enter a valid temperature.", temperatureString);
+					temperatureString = Console.ReadLine();
+					temperature = Convert.ToInt32(temperatureString);
+				}
+			sum += temperature;
+			count++;
+			Console.Write ("Enter a temperature between -20 and 130 degrees Fahrenheit. When finished enter ##.");
+			temperatureString = Console.ReadLine();
 			}
-		sum += temperature;
-		count++;
-		Console.Write ("Enter a temperature between -20 and 130 degrees Fahrenheit. When finished enter ##.");
-		temperatureString = Console.ReadLine();
+		
+			if(temperatureString == "##")
+			{
+				average = sum / count;
+				Console.WriteLine("The number of temperatures entered was {0} with an average of {1}.", count, average);
+			}
 		}
-		
-		
-		if(temperatureString == "##")
-		{
-			average = sum / count;
-			Console.WriteLine("The number of temperatures entered was {0} with an average of {1}.", count, average);
-		}
-		
 	}
 }
