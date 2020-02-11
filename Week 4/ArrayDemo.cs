5921255775888d35ee7a2853e52f853759cfa411
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-10 20:44:47 -0500

    Update ArrayDemo.cs

--- Week 4/ArrayDemo.cs
+++ Week 4/ArrayDemo.cs
@@ -39,20 +39,20 @@ namespace ArrayDemo
 			//Continual do loop in order to continue running program until terminated
 			do
 			{
-				Console.WriteLine("---------------------------");
-				Console.WriteLine("ENTER YOUR DESIRED OUTOCOME");
-				Console.WriteLine("---------------------------");
-				Console.WriteLine("For rank 1 to 10  **ENTER 1**");
-				Console.WriteLine("For rank 10 to 1  **ENTER 2**");
-				Console.WriteLine("For specific rank **ENTER 3**");
-				Console.WriteLine("To quit program   **ENTER 000**");
+				WriteLine("---------------------------");
+				WriteLine("ENTER YOUR DESIRED OUTOCOME");
+				WriteLine("---------------------------");
+				WriteLine("For rank 1 to 10  **ENTER 1**");
+				WriteLine("For rank 10 to 1  **ENTER 2**");
+				WriteLine("For specific rank **ENTER 3**");
+				WriteLine("To quit program   **ENTER 000**");
 
-				userInput = Convert.ToInt32(Console.ReadLine());
+				userInput = Convert.ToInt32(ReadLine());
 
 				//Correct user input check
 				if (userInput != 000 && (userInput < 1 || userInput > 3))
 				{
-					Console.WriteLine("*** INVALID INPUT *** Enter 1,2,3 or 000 to terminate");
+					WriteLine("*** INVALID INPUT *** Enter 1,2,3 or 000 to terminate");
 				}
 
 				//Use cases to determine user input outcome
@@ -60,36 +60,36 @@ namespace ArrayDemo
 				{
 					//For loop to display index 0 to 9
 					case 1:
-						Console.WriteLine("\n");
-						Console.WriteLine("Jersey numbers ranked 1 - 10");
+						WriteLine("\n");
+						WriteLine("Jersey numbers ranked 1 - 10");
 						for (int i = 0; i < jerseyNumbers.Length; i++)
 						{
-							Console.Write(jerseyNumbers[i] + " ");
+							Write(jerseyNumbers[i] + " ");
 						}
-						Console.WriteLine("\n\n");
+						WriteLine("\n\n");
 						break;
 					//For loop to display index 9 to 0
 					case 2:
-						Console.WriteLine("\n");
-						Console.WriteLine("Jersey numbers ranked 10 - 1");
+						WriteLine("\n");
+						WriteLine("Jersey numbers ranked 10 - 1");
 						for (int i = 9; i >= 0; i--)
 						{
-							Console.Write(jerseyNumbers[i] + " ");
+							Write(jerseyNumbers[i] + " ");
 						}
-						Console.WriteLine("\n\n");
+						WriteLine("\n\n");
 						break;
 					//If else to determine specific index
 					case 3:
-						Console.WriteLine(" ______");
-						Console.WriteLine("Enter a rank from 1-10");
+						WriteLine(" ______");
+						WriteLine("Enter a rank from 1-10");
 						//Allow a second prompt to determine index
-						int extraInput = Convert.ToInt32(Console.ReadLine());
+						int extraInput = Convert.ToInt32(ReadLine());
 						if (extraInput >= 1 && extraInput <= 10)
 							//Subtract one from userinput in order to return proper index
 							//User friendly to eliminate people not using zero indexing
-							Console.WriteLine("\nJersey number {0} has a rank of {1}\n\n", jerseyNumbers[extraInput - 1], extraInput);
+							WriteLine("\nJersey number {0} has a rank of {1}\n\n", jerseyNumbers[extraInput - 1], extraInput);
 						else
-							Console.WriteLine("\n*** INVALID INPUT ***\n\n");
+							WriteLine("\n*** INVALID INPUT ***\n\n");
 						break;
 				}
 			}
