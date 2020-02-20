443b674aa13f648c4fb0f2f77236f10fe2c388e5
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-20 14:37:16 -0500

    Add files via upload

---
+++ Week 5/ShirtDemo.cs
@@ -0,0 +1,75 @@
+﻿/*
+*
+* Student Jaime Rodriguez
+* Bellevue University
+* CIS355
+*
+*/
+/* File ShirtDemo.cs */
+/*
+* Assignment 5.3
+* Purpose: Create two shirts from shirt class.
+* Display the shirts characteristics.
+*
+* To compile the program:
+* csc ShirDemo.cs
+*
+* To execute the program:
+* ShirDemo.exe (.exe optional)
+*
+*/
+
+using System;
+using static System.Console;
+
+namespace ShirtDemo
+{
+    class ShirtDemo
+    {
+        static void Main(string[] args)
+        {
+            //Create two shirts
+            Shirt shirtOne = new Shirt();
+            Shirt shirtTwo = new Shirt();
+            
+
+            WriteLine("\n{0,0} {1,10} {2,6} {3,6}", "Order #", "Material", "Color", "Size");
+            shirtOne.Display();
+            shirtTwo.Display();
+        }
+    }
+
+    class Shirt
+    {
+
+        public string Number { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+
+        //Constructor to be run everytime a shirt is made
+        public Shirt()
+        {
+            AskQuestion();
+        }
+
+        //Receive shirt characteristics from user
+        public void AskQuestion()
+        {
+            Write("Enter order number: ");
+            Number = ReadLine();
+            Write("What material would you like: ");
+            Material = ReadLine();
+            Write("What color would you like: ");
+            Color = ReadLine();
+            Write("What size would you like: ");
+            Size = ReadLine();
+        }
+
+        //Format of shirt display
+        public void Display()
+        {
+            WriteLine("{0,0} {1,14} {2,8} {3,6}", Number, Material, Color, Size);
+        }
+    }
+}
