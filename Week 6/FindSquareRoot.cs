5e5c83db4f61afa8d06597bba3d0eaaf62fc8f5b
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-25 10:22:48 -0500

    Add files via upload

---
+++ Week 6/FindSquareRoot.cs
@@ -0,0 +1,93 @@
+﻿/*
+*
+* Student Jaime Rodriguez
+* Bellevue University
+* CIS355
+*
+*/
+/* File FindSquareRoot.cs */
+/*
+* Assignment 6.3
+* Purpose: Return the square root value
+* of a users input
+*
+* To compile the program:
+* csc FindSquareRoot.cs
+*
+* To execute the program:
+* FindSquareRoot.exe (.exe optional)
+*
+*/
+
+using System;
+using static System.Console;
+
+namespace FindSquareRoot
+{
+    class FindSquareRoot
+    {
+        static void Main(string[] args)
+        {
+
+            SquareRoot sqrt = new SquareRoot();
+            try
+            {
+                Write("Enter a number to find squre root of: ");
+                sqrt.UserInput = Convert.ToDouble(ReadLine());
+                sqrt.SquareRootValue = Math.Sqrt(sqrt.UserInput);
+            }
+
+            //When user input is not a number catch exception
+            catch (FormatException e)
+            {
+                sqrt.SquareRootValue = 0;
+                WriteLine(e.Message);
+                WriteLine("Input value should be a number");
+            }
+
+            //When user input is negative catch exception
+            catch (NegativeNumberInput e)
+            {
+                sqrt.SquareRootValue = 0;
+                WriteLine(e.Message);
+            }
+            WriteLine("\nThe square root of your number is: {0}", sqrt.SquareRootValue);
+
+
+        }
+    }
+
+    //Create ApplicationException
+    class NegativeNumberInput : Exception
+    {
+        //Message to be displayed
+        private static string msg = "Input value can not be negative";
+        public NegativeNumberInput() : base(msg)
+        {
+        }
+    }
+
+    class SquareRoot
+    {
+        public double userInput;
+        public double SquareRootValue { get; set; }
+
+        public double UserInput
+        {
+            get
+            {
+                return userInput;
+            }
+            set
+            {
+                //Creating a new instance of our created exception
+                if (value < 0)
+                {
+                    NegativeNumberInput nni = new NegativeNumberInput();
+                    throw (nni);
+                }
+                userInput = value;
+            }
+        }
+    }
+}
