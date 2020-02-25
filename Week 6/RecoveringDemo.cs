0f94335a99afe95d62a25e7145621dc94f3d3b7e
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-02-25 10:18:04 -0500

    Add files via upload

---
+++ Week 6/RecoveringDemo.cs
@@ -0,0 +1,98 @@
+﻿/*
+*
+* Student Jaime Rodriguez
+* Bellevue University
+* CIS355
+*
+*/
+/* File RecoveringDemo.cs */
+/*
+* Assignment 6.2
+* Purpose: Based on the the type of class
+* (patient, upholsterer, football player)
+* the output will display the class's
+* "Recover()" status
+*
+* To compile the program:
+* csc RecoveringDemo.cs
+*
+* To execute the program:
+* RecoveringDemo.exe (.exe optional)
+*
+*/
+
+using System;
+using static System.Console;
+
+namespace RecoveringDemo
+{
+    class RecoveringDemo
+    {
+        static void Main(string[] args)
+        {
+            Patient jaime = new Patient("Jaime", "patient");
+            Upholsterer fred = new Upholsterer("Fred", "upholsterer");
+            FootballPlayer russellWilson = new FootballPlayer("Russell Wilson", "football player");
+
+            WriteLine(jaime.Name + "'s curent status as a " + jaime.Type + ": " + jaime.Recovery());
+            WriteLine(fred.Name + "'s curent status as a " + fred.Type + ": " + fred.Recovery());
+            WriteLine(russellWilson.Name + "'s curent status as a " + russellWilson.Type + ": " + russellWilson.Recovery());
+        }
+    }
+
+    class Patient : IRecoverable
+    {
+        public Patient(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public string Recovery()
+        {
+            return "Getting Better";
+        }
+    }
+
+    class Upholsterer : IRecoverable 
+    {
+        public Upholsterer(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public string Recovery()
+        {
+            return "Fully Recovered";
+        }
+    }
+
+    class FootballPlayer : IRecoverable
+    {
+        public FootballPlayer(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public string Recovery()
+        {
+            return "In Concussion Protocol";
+        }
+    }
+
+    public interface IRecoverable
+    {
+        string Recovery();
+    }
+}
