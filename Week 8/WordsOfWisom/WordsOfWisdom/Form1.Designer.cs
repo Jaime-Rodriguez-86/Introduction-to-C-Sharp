8090039472454b1d4fb1c7e26a0dca0d7e41fd43
Jaime Rodriguez <57820323+Jaime-Rodriguez-86@users.noreply.github.com>
2020-03-12 12:41:49 -0400

    Add files via upload

---
+++ Week 8/WordsOfWisom/WordsOfWisdom/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WordsOfWisdom
+{
+    partial class wisdomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.strengthLabelHeader = new System.Windows.Forms.Label();
+            this.strengthLabel = new System.Windows.Forms.Label();
+            this.honorLabelHeader = new System.Windows.Forms.Label();
+            this.honorLabel = new System.Windows.Forms.Label();
+            this.trustLabelHeader = new System.Windows.Forms.Label();
+            this.trustLabel = new System.Windows.Forms.Label();
+            this.fearLabelHeader = new System.Windows.Forms.Label();
+            this.fearLabel = new System.Windows.Forms.Label();
+            this.formHeaderLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // strengthLabelHeader
+            // 
+            this.strengthLabelHeader.AutoSize = true;
+            this.strengthLabelHeader.BackColor = System.Drawing.Color.Black;
+            this.strengthLabelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strengthLabelHeader.ForeColor = System.Drawing.Color.White;
+            this.strengthLabelHeader.Location = new System.Drawing.Point(73, 119);
+            this.strengthLabelHeader.Name = "strengthLabelHeader";
+            this.strengthLabelHeader.Size = new System.Drawing.Size(111, 29);
+            this.strengthLabelHeader.TabIndex = 0;
+            this.strengthLabelHeader.Text = "Strength";
+            // 
+            // strengthLabel
+            // 
+            this.strengthLabel.AutoSize = true;
+            this.strengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strengthLabel.Location = new System.Drawing.Point(77, 148);
+            this.strengthLabel.MinimumSize = new System.Drawing.Size(200, 100);
+            this.strengthLabel.Name = "strengthLabel";
+            this.strengthLabel.Size = new System.Drawing.Size(200, 100);
+            this.strengthLabel.TabIndex = 1;
+            this.strengthLabel.MouseLeave += new System.EventHandler(this.strengthLabel_MouseLeave);
+            this.strengthLabel.MouseHover += new System.EventHandler(this.strengthLabel_MouseHover);
+            // 
+            // honorLabelHeader
+            // 
+            this.honorLabelHeader.AutoSize = true;
+            this.honorLabelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.honorLabelHeader.ForeColor = System.Drawing.Color.White;
+            this.honorLabelHeader.Location = new System.Drawing.Point(75, 301);
+            this.honorLabelHeader.Name = "honorLabelHeader";
+            this.honorLabelHeader.Size = new System.Drawing.Size(84, 29);
+            this.honorLabelHeader.TabIndex = 2;
+            this.honorLabelHeader.Text = "Honor";
+            // 
+            // honorLabel
+            // 
+            this.honorLabel.AutoSize = true;
+            this.honorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.honorLabel.Location = new System.Drawing.Point(77, 330);
+            this.honorLabel.MinimumSize = new System.Drawing.Size(200, 100);
+            this.honorLabel.Name = "honorLabel";
+            this.honorLabel.Size = new System.Drawing.Size(200, 100);
+            this.honorLabel.TabIndex = 3;
+            this.honorLabel.MouseLeave += new System.EventHandler(this.honorLabel_MouseLeave);
+            this.honorLabel.MouseHover += new System.EventHandler(this.honorLabel_MouseHover);
+            // 
+            // trustLabelHeader
+            // 
+            this.trustLabelHeader.AutoSize = true;
+            this.trustLabelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trustLabelHeader.ForeColor = System.Drawing.Color.White;
+            this.trustLabelHeader.Location = new System.Drawing.Point(365, 119);
+            this.trustLabelHeader.Name = "trustLabelHeader";
+            this.trustLabelHeader.Size = new System.Drawing.Size(73, 29);
+            this.trustLabelHeader.TabIndex = 4;
+            this.trustLabelHeader.Text = "Trust";
+            // 
+            // trustLabel
+            // 
+            this.trustLabel.AutoSize = true;
+            this.trustLabel.Location = new System.Drawing.Point(367, 148);
+            this.trustLabel.MinimumSize = new System.Drawing.Size(200, 100);
+            this.trustLabel.Name = "trustLabel";
+            this.trustLabel.Size = new System.Drawing.Size(200, 100);
+            this.trustLabel.TabIndex = 5;
+            this.trustLabel.MouseLeave += new System.EventHandler(this.trustLabel_MouseLeave);
+            this.trustLabel.MouseHover += new System.EventHandler(this.trustLabel_MouseHover);
+            // 
+            // fearLabelHeader
+            // 
+            this.fearLabelHeader.AutoSize = true;
+            this.fearLabelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fearLabelHeader.ForeColor = System.Drawing.Color.White;
+            this.fearLabelHeader.Location = new System.Drawing.Point(365, 301);
+            this.fearLabelHeader.Name = "fearLabelHeader";
+            this.fearLabelHeader.Size = new System.Drawing.Size(67, 29);
+            this.fearLabelHeader.TabIndex = 6;
+            this.fearLabelHeader.Text = "Fear";
+            // 
+            // fearLabel
+            // 
+            this.fearLabel.AutoSize = true;
+            this.fearLabel.Location = new System.Drawing.Point(367, 330);
+            this.fearLabel.MinimumSize = new System.Drawing.Size(200, 100);
+            this.fearLabel.Name = "fearLabel";
+            this.fearLabel.Size = new System.Drawing.Size(200, 100);
+            this.fearLabel.TabIndex = 7;
+            this.fearLabel.MouseLeave += new System.EventHandler(this.fearLabel_MouseLeave);
+            this.fearLabel.MouseHover += new System.EventHandler(this.fearLabel_MouseHover);
+            // 
+            // formHeaderLabel
+            // 
+            this.formHeaderLabel.AutoSize = true;
+            this.formHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formHeaderLabel.ForeColor = System.Drawing.Color.White;
+            this.formHeaderLabel.Location = new System.Drawing.Point(158, 9);
+            this.formHeaderLabel.Name = "formHeaderLabel";
+            this.formHeaderLabel.Size = new System.Drawing.Size(260, 33);
+            this.formHeaderLabel.TabIndex = 8;
+            this.formHeaderLabel.Text = "Words of Wisdom";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(161, 51);
+            this.label1.MinimumSize = new System.Drawing.Size(260, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Hover Under Category To See Quote";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // wisdomForm
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(626, 484);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formHeaderLabel);
+            this.Controls.Add(this.fearLabel);
+            this.Controls.Add(this.fearLabelHeader);
+            this.Controls.Add(this.trustLabel);
+            this.Controls.Add(this.trustLabelHeader);
+            this.Controls.Add(this.honorLabel);
+            this.Controls.Add(this.honorLabelHeader);
+            this.Controls.Add(this.strengthLabel);
+            this.Controls.Add(this.strengthLabelHeader);
+            this.Name = "wisdomForm";
+            this.Text = "Provider of Wisdom";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelTwo;
+        private System.Windows.Forms.Label strengthLabelHeader;
+        private System.Windows.Forms.Label strengthLabel;
+        private System.Windows.Forms.Label honorLabelHeader;
+        private System.Windows.Forms.Label honorLabel;
+        private System.Windows.Forms.Label trustLabelHeader;
+        private System.Windows.Forms.Label trustLabel;
+        private System.Windows.Forms.Label fearLabelHeader;
+        private System.Windows.Forms.Label fearLabel;
+        private System.Windows.Forms.Label formHeaderLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week 8/WordsOfWisom/WordsOfWisdom/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordsOfWisdom
+{
+    public partial class wisdomForm : Form
+    {
+        public wisdomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void strengthLabel_MouseHover(object sender, EventArgs e)
+        {
+            strengthLabel.Text = "THAT WHICH DOES\nNOT KILL US MAKES\nUS STRONGER";
+            strengthLabel.Font = new System.Drawing.Font("Sans Sarif", 14F, System.Drawing.FontStyle.Italic);
+            strengthLabel.ForeColor = Color.White;
+        }
+        private void strengthLabel_MouseLeave(object sender, EventArgs e)
+        {
+            strengthLabel.ForeColor = Color.Black;
+        }
+
+        
+        private void honorLabel_MouseHover(object sender, EventArgs e)
+        {
+            honorLabel.Text = "IT IS NOT TITLES THAT\nHONOR MEN BUT MEN\nTHAT HONOR TITLES";
+            honorLabel.Font = new System.Drawing.Font("Sans Sarif", 14F, System.Drawing.FontStyle.Italic);
+            honorLabel.ForeColor = Color.White;
+        }
+
+        private void honorLabel_MouseLeave(object sender, EventArgs e)
+        {
+            honorLabel.ForeColor = Color.Black;
+        }
+
+        private void trustLabel_MouseHover(object sender, EventArgs e)
+        {
+            trustLabel.Text = "LOVE ALL. TRUST FEW.\nDO WRONG TO NONE";
+            trustLabel.Font = new System.Drawing.Font("Sans Sarif", 14F, System.Drawing.FontStyle.Italic);
+            trustLabel.ForeColor = Color.White;
+        }
+
+        private void trustLabel_MouseLeave(object sender, EventArgs e)
+        {
+            trustLabel.ForeColor = Color.Black;
+        }
+
+        private void fearLabel_MouseHover(object sender, EventArgs e)
+        {
+            fearLabel.Text = "FEAR HAS A LARGE\nSHADOW, BUT HE\nHIMSELF IS SMALL";
+            fearLabel.Font = new System.Drawing.Font("Sans Sarif", 14F, System.Drawing.FontStyle.Italic);
+            fearLabel.ForeColor = Color.White;
+        }
+
+        private void fearLabel_MouseLeave(object sender, EventArgs e)
+        {
+            fearLabel.ForeColor = Color.Black;
+        }
+
+        
+    }
+}
